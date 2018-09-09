b327e413f0cf684eb79c07a9893ace31e4004214
Bruno Garcia <bruno.garcia@ltplabs.com>
2018-09-09 22:45:06 +0100

    refactor test and add test GET /todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,9 +4,21 @@ const request = require('supertest');
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
+const todos = [{
+    text: 'First test todo'
+}, {
+    text: 'Second test todo'
+}];
+
+beforeEach((done) => {
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
+});
+
 describe('POST /todos', () => {
     it('should create a new todo', (done) => {
-        var text = 'Test todo test';
+        var text = 'Test todo text';
 
         request(app)
             .post('/todos')
@@ -20,9 +32,9 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.findOne({ text: text }).then((todo) => {
-                    console.log(todo.text);
-                    expect(todo.text).toBe(text);
+                Todo.find({ text }).then((todos) => {
+                    expect(todos.length).toBe(1);
+                    expect(todos[0].text).toBe(text);
                     done();
                 }).catch((e) => done(e));
             });
@@ -38,10 +50,22 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find().then((todo) => {
-                    expect(todo.text).toBe(undefined);
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
             });
+    });
+});
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.docs.length).toBe(2);
+            })
+            .end(done);
     });
 });
